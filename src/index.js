370546987a7628a561965a545545c33bf42d4a29
Tandazwa Ngqobololo <tandi.ngqob@gmail.com>
2024-06-26 16:48:44 +0000

    updated js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let currentTemperature = response.data.current;
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
+  let currentDateELement = document.querySelector("#current-date");
+  let currentDate = new Date();
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let IconElement = document.querySelector("#icon");
 
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = searchInputElement.value;
   descriptionElement.innerHTML = response.data.condition.description;
+  currentDateELement.innerHTML = formatDate(currentDate);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   IconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
-  getWeatherInfo(searchInputElement.value);
-}
-function getWeatherInfo(city) {
-  let apiKey = "f5c79472073a0of17823941te0ebda6a";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(showTemperature);
 }
 
-function showTemperature(response) {
-  let temperatureElement = document.querySelector("#current-temperature");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  let city = response.data.current;
-
-  console.log(`Temperature: ${temperature}°C`);
-}
-getWeatherInfo("Paris");
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -56,10 +45,20 @@ function formatDate(date) {
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
+function getWeatherInfo(city) {
+  let apiKey = "f5c79472073a0of17823941te0ebda6a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  getWeatherInfo(searchInputElement.value);
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
-
-currentDateELement.innerHTML = formatDate(currentDate);
+getWeatherInfo("Paris");
